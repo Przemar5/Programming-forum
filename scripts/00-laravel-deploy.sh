a5edbf0bedb2e254e47969b3d2a0c0084683dbb5
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-16 23:05:11 +0100

    Add server deploy scripts

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Running migrations..."
+php bin/console do:mi:mi
+php bin/console do:fi:lo
