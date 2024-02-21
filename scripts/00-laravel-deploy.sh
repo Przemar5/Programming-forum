15a892e65e5f7929120c6db1ccf7f3eda3c242fa
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-21 23:37:06 +0100

    Repair deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -7,11 +7,12 @@ cat composer.json
 chmod a+w -R vendor
 # composer update symfonycasts/reset-password-bundle
 echo "composer require -vvv hirak/prestissimo"
-composer require -vvv hirak/prestissimo
+composer require -vvv --no-interaction hirak/prestissimo
 ls -l vendor
-composer install --no-dev -vvv --working-dir=/var/www/html
+composer install --no-dev --no-interaction -vvv --working-dir=/var/www/html
 chmod a+w -R vendor
 composer dump-autoload
+
 chmod a+w -R vendor
 ls -l
 ls -l vendor/
