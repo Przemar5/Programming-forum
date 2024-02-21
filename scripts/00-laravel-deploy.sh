190c788c27abb7a30f6e41ecd93b6af57d9f6e96
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-21 15:10:48 +0100

    Repair deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,14 +3,14 @@ echo "Running composer"
 composer global require hirak/prestissimo
 cd /var/www/html
 composer clear-cache
-composer diagnose
-# composer reinstall
 cat composer.json
-composer update symfonycasts/reset-password-bundle
+composer update
+# composer update symfonycasts/reset-password-bundle
 composer install --no-dev -vvv --working-dir=/var/www/html
 composer dump-autoload
 # chmod o+w
 ls -l
+ls -l vendor/
 
 echo "Running migrations..."
 php bin/console do:mi:mi
