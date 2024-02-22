200693c539edfc28a171d1ba0d9f8ae99dbbcf45
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-22 16:28:59 +0100

    Repair deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,11 +5,15 @@ cd /var/www/html
 composer clear-cache
 cat composer.json
 chmod a+w -R vendor
+echo "Diagnose Composer"
+composer diagnose
 # composer update symfonycasts/reset-password-bundle
 echo "composer require -vvv hirak/prestissimo"
 composer require -vvv --no-interaction hirak/prestissimo
 ls -l vendor
 rm -r vendor
+echo "Memory:"
+php -r "echo ini_get('memory_limit').PHP_EOL;"
 composer install --no-dev --no-interaction -vvv --working-dir=/var/www/html
 chmod a+w -R vendor
 # composer dump-autoload
