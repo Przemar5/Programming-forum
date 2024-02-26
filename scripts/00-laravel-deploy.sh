cee0f3d3e02f41108cff4092c895a8e2181690b3
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-26 17:45:39 +0100

    Repair deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,38 +3,13 @@ echo "Running composer"
 composer --version
 # composer global require hirak/prestissimo
 # cd /var/www/html
-# composer clear-cache
-# cat composer.json
-# chmod a+w -R vendor
-# echo "Diagnose Composer"
-# composer diagnose
-# composer update symfonycasts/reset-password-bundle
-# echo "composer require -vvv hirak/prestissimo"
-# php -d memory_limit=-1 /usr/bin/composer require --no-cache -vvv --no-interaction hirak/prestissimo
-# ls -l vendor
-# rm -r vendor
-# echo "Memory:"
-# php -r "echo ini_get('memory_limit').PHP_EOL;"
-# echo "Error handler:"
-# php -d memory_limit=-1 /usr/bin/composer require vanilla/nbbc --no-cache --no-scripts --no-interaction -vvv --working-dir=/var/www/html
-# echo "Test:"
-# php -d memory_limit=-1 /usr/bin/composer require chriskonnertz/bbcode --no-scripts --no-interaction -vvv --working-dir=/var/www/html
-# echo "All:"
-# composer install --help
 composer install --working-dir=/var/www/html
-# php -d max_execution_time=-1 /usr/bin/composer install -vvv --working-dir=/var/www/html
-# echo "Which composer"
-# which composer
-# chmod a+w -R vendor
-# composer dump-autoload
-# ls composer.lock
 
-# chmod a+w -R vendor
-ls -l
-ls -l vendor/
+# ls -l
+# ls -l vendor/
 
-# echo "Running migrations..."
-# php bin/console do:mi:mi
+echo "Running migrations..."
+php bin/console do:mi:mi
 # php bin/console do:fi:lo
 
 # bash
