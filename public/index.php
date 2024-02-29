af93c51bfe0e678b353e06fffeaefd0e5c0c68f6
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-29 22:52:52 +0100

    Repair deploy

--- public/index.php
+++ public/index.php
@@ -20,22 +20,22 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 // dd(12345678);
-// dump(1);
+dump(1);
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
-// dump(2);
+dump(2);
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-// dump(3);
+dump(3);
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
-// dump(4);
+dump(4);
 $response = $kernel->handle($request);
 dump(5);
 $response->send();
-// dump(6);
+dump(6);
 $kernel->terminate($request, $response);
 } catch (\Exception $e) {
     dd($e);
