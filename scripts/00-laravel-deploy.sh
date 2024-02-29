fac9254847298702b495c3ca569d85e9f086e720
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-02-29 23:34:45 +0100

    Repair deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-# composer global require hirak/prestissimo
-# cd /var/www/html
 composer install --working-dir=/var/www/html
 
-# ls -l
-# ls -l vendor/
-
 echo "Running migrations..."
-# php bin/console do:mi:mi
-# php bin/console do:fi:lo
-
-# bash
+php bin/console do:mi:mi
