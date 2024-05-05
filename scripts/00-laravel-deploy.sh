85e28de1d14f639d2c24c06aeb4c59d664dbd02e
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-05-06 01:26:59 +0200

    Add fixtures loading

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --working-dir=/var/www/html
+composer require doctrine/doctrine-fixtures-bundle
 
 echo "Running migrations..."
 php bin/console do:mi:mi
