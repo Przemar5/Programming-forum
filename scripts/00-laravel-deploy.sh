54ca813b183532cc895b27de95f50695f4f8b0fb
Przemek_Krogulski <przemyslaw.krogulski@innovationsoftware.pl>
2024-05-07 18:19:54 +0200

    Fix deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --working-dir=/var/www/html
-composer require doctrine/doctrine-fixtures-bundle
+# composer require doctrine/doctrine-fixtures-bundle --dev
 
 echo "Running migrations..."
 php bin/console do:mi:mi
-php bin/console doctrine:fixtures:load --fixtures=src/DataFixtures/
